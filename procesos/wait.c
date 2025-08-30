57ee82924be8c65038f5c49bc11194eeaf3a2402
Lucas Flores <lucasnicolasflores@gmail.com>
2025-08-30 13:18:25 +0000

    Ejercicio de :cree tres procesos hijos usando un bucle for y la función fork().

--- procesos/wait.c
+++ procesos/wait.c
@@ -4,51 +4,65 @@
 #include <unistd.h>
 #include <stdio.h>
 
+void saltoLinea() {
+    printf(" \n");
+}
+
 int imprimirInfoProceso(int tiempo_espera) {
     printf(
         "mi PID es %d \n"
-        "Desperte en %d segundos \n"
-        "------- Separador ------- \n", 
-        getpid(), tiempo_espera
+        "mi PID padre es %d \n"
+        "Desperte en %d segundos \n", 
+        getpid(), getppid(), tiempo_espera
     );
+    saltoLinea();
 
     return 0;
 }
 
 int main() {
-    pid_t proceso;
-    pid_t proceso_padre;
+    pid_t proceso, proceso_padre;
     int loops = 3;
     int tiempo_espera = 2;
-    int tiempo_espera_ant = tiempo_espera;
-
+    
     proceso_padre = getpid(); // Guaramos el dato de cual es el proceso padre
+
+    // Confirmamos cual es el proceso padre (Simple referencia para mi)
+    saltoLinea();
     printf("Este es el proceso padre (PID-%d) \n", proceso_padre);
     imprimirInfoProceso(1);
-    
+    saltoLinea();
+
     // revisar la actividad de SO para esto con el WAIT y el sleeo
     for (int i = 0; i < loops; i++) {
-        
         proceso = fork();
-        sleep(tiempo_espera);
-
-        tiempo_espera_ant = tiempo_espera; // guardo el tiempo para una impresion nueva
-        tiempo_espera = tiempo_espera + 2;
         if (proceso == 0) {
+            sleep(tiempo_espera);
+
+            int tiempo_despertar = tiempo_espera + 2; // guardo el tiempo para una impresion nueva
+            // Muestro la info del proceso
+            imprimirInfoProceso(tiempo_espera);
             break;
         }
-        
+        tiempo_espera = tiempo_espera + 2;
     }
 
-    imprimirInfoProceso(tiempo_espera_ant);
-
-    waitpid(proceso, NULL, 0);
+    // el proceso en caso de tener un hijo siempre espera a que este termine para finalizarse
+    // PROCESO 1 <- Epera al proceso 1.2
+    // |--- PROCESO 1.2 <- espera al proceso 1.2.1
+    // |---|--- PROCESO 1.2.1
+    waitpid(
+        proceso, // le facilitamos el PID del proceso a esperar
+        NULL, // Se indica que no importa el como termino el proceso pero cuando lo haga se mate el proceso
+        0
+    );
     
     if (getpid() == proceso_padre) {
+        printf("----------- Separado ----------- \n");
+        saltoLinea();
         printf("Este es el fin del proceso padre (PID-%d) \n", proceso_padre);
-
+        saltoLinea();
     };
-    
 
     return 0;
 }
